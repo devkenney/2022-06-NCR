fd44e3f2b80012b4efd9614b7936107ad9be24be
Devin Kenney <devin.kenney111@gmail.com>
2022-09-14 10:55:34 -0400

    added mongoose stuff (week 8 day 3)

--- fruits/server.js
+++ fruits/server.js
@@ -2,6 +2,16 @@ const express = require('express');
 const app = express();
 const fruits = require('./models/fruits');
 const veggies = require('./models/veggies');
+require('dotenv').config();
+const mongoose = require('mongoose')
+
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
+mongoose.connection.once('open', ()=> {
+  console.log('connected to mongo');
+});
 
 app.set('view engine', 'jsx');
 app.engine('jsx', require('express-react-views').createEngine());
